294e0cbd313f1004fabd4a585aa27ed9c5388d89
Ubuntu <ubuntu@ip-172-31-46-61.ap-southeast-4.compute.internal>
2025-11-05 18:15:00 +0000

    trying to fix conv

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -54,6 +54,9 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     # Can assume multiple of 128 to avoid using mask
     assert in_channels % 128 == out_channels % 128 == 0
 
+    # TODO: add pooling
+    assert pool_size == 1
+
     # Can assume one PSUM bank can at least fit one row of the pixels
     assert nl.tile_size.gemm_moving_fmax >= out_width
 
@@ -67,12 +70,56 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     # Various tiling dimensions (You may want to define more of them)
     c_in_pmax = nl.tile_size.pmax
     n_tiles_c_in = in_channels // c_in_pmax
+    TILE_M = nl.tile_size.gemm_stationary_fmax  # 128
+    TILE_K = nl.tile_size.pmax  # 128
+    TILE_N = nl.tile_size.gemm_moving_fmax  # 512
+    
+    output = nl.ndarray(shape=(out_channels, out_height * out_width),
+                        dtype=X.dtype,
+                        buffer=nl.hbm,)
+
+    # Define matmul shapes
+    K = in_channels
+    M = out_channels
+    N = out_height * out_width
 
     # Process the images in batches
     for b in nl.affine_range(batch_size):
-        raise RuntimeError("Please fill your implementation of computing convolution"
-                           " of X[b] with the weights W and bias b, followed by a"
-                           " maxpool and store the result in X_out[b]")
 
+        for m in nl.affine_range(M // TILE_M):
+
+            for n in nl.affine_range(N // TILE_N):
+
+                res_psum = nl.zeros((TILE_M, TILE_N), nl.float32, buffer=nl.psum)
+                for k in nl.affine_range(K // TILE_K):
+                    lhsT_tile = nl.ndarray((TILE_M, TILE_K), dtype=W.dtype, buffer=nl.sbuf)
+                    rhs_tile = nl.ndarray((TILE_K, TILE_N), dtype=X.dtype, buffer=nl.sbuf)
+                    # Iterate over the filter height
+                    for i in range(filter_height):
+                        # Iterate over the filter width
+                        for j in range(filter_width):
+                            w_block = W[m * TILE_M:(m + 1) * TILE_M, k * TILE_K:(k + 1) * TILE_K, i, j]
+
+                            # Shift the Input tensor by (i, j) to align with the filter's current position
+                            input_shifted = X[b, :, i:i+out_height, j:j+out_width] #TODO: integer indexing?
+                            # Flatten the shifted input before the matmul
+                            input_shifted_flat = input_shifted.reshape((in_channels, out_height * out_width))
+
+                            # Load tiles from lhsT and rhs
+                            nisa.dma_copy(dst=lhsT_tile, src=w_block)
+                            nisa.dma_copy(dst=rhs_tile, src=input_shifted_flat[k * TILE_K:(k + 1) * TILE_K, n * TILE_N:(n + 1) * TILE_N])
+
+                            # Accumulate partial-sums into PSUM
+                            res_psum += nisa.nc_matmul(lhsT_tile, rhs_tile, is_transpose=True)
+
+                            # accumulate in total output
+                # Copy block m,n to output. PSUM->SBUF->output_flat
+                res_sb = nisa.tensor_copy(res_psum, dtype=output_flat.dtype)
+                nisa.dma_copy(dst=output_flat[m * TILE_M:(m + 1) * TILE_M, n * TILE_N:(n + 1) * TILE_N], src=res_sb)
+        # TODO: implement pooling
+        out_img = output_flat.reshape((out_channels, out_height, out_width))
+        nisa.dma_copy(dst=X_out[b, :, :, :], src=out_img)
     return X_out
+
+
 
